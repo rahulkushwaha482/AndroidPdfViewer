1909887dc8d13beb4ce6ad7da32301298d27344e
rahul.kushwaha@techconfer.in <techconfer@2020>
2021-08-18 17:15:58 +0530

    Android Pdf File viewer online using Url

---
+++ app/src/main/java/com/pdfpreviewapp/SecondActivity.java
@@ -0,0 +1,51 @@
+package com.pdfpreviewapp;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import androidx.appcompat.app.AppCompatActivity;
+import com.github.barteksc.pdfviewer.PDFView;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class SecondActivity extends AppCompatActivity {
+    PDFView pdfView;
+    ProgressBar progressCircular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        pdfView = findViewById(R.id.pdfView);
+        progressCircular = findViewById(R.id.progress_circular);
+        progressCircular.setIndeterminate(true);
+        new RetrivePDFStream().execute();
+    }
+
+    class RetrivePDFStream extends AsyncTask<Void, Void, Void> {
+
+        protected Void doInBackground(Void... voids) {
+            try {
+                InputStream input = new URL("https://livefitlife.in/LFL/public/uploads/generic/60dd98176a261DietPreparation&Grocerylist.pdf").openStream();
+                pdfView.fromStream(input).load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressCircular.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            progressCircular.setVisibility(View.GONE);
+        }
+    }
+}
